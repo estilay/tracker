54540bbf09fca07754d3e5709077a310e77671ec
estilay <kavalenkamatsvei@gmail.com>
2026-08-09 02:43:16 +0300

    feat: implement ScheduleViewController with ViewCell

---
+++ Tracker/Models/Schedule.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+enum Schedule: String, CaseIterable {
+    case monday = "Понедельник"
+    case tuesday = "Вторник"
+    case wednesday = "Среда"
+    case thursday = "Четверг"
+    case friday = "Пятница"
+    case saturday = "Суббота"
+    case sunday = "Воскресенье"
+    
+    var short: String {
+        switch self {
+        case .monday: return "Пн"
+        case .tuesday: return "Вт"
+        case .wednesday: return "Ср"
+        case .thursday: return "Чт"
+        case .friday: return "Пт"
+        case .saturday: return "Сб"
+        case .sunday: return "Вс"
+        }
+    }
+    
+    static func from(weekday: Int) -> Schedule {
+        switch weekday {
+        case 1: return .sunday
+        case 2: return .monday
+        case 3: return .tuesday
+        case 4: return .wednesday
+        case 5: return .thursday
+        case 6: return .friday
+        case 7: return .saturday
+        default: return .monday
+        }
+    }
+}
