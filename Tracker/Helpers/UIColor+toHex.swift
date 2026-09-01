e0ce79ff73318ebd1ded6af0c0ffd922e8015e48
estilay <kavalenkamatsvei@gmail.com>
2026-09-01 16:51:57 +0300

    feat: add UIColor extension for entities attribute type

---
+++ Tracker/Helpers/UIColor+toHex.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+// MARK: - UIColor Extension
+extension UIColor {
+    func toHex() -> String {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        return String(format: "#%02X%02X%02X",
+                     Int(red * 255),
+                     Int(green * 255),
+                     Int(blue * 255))
+    }
+    
+    convenience init?(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+        
+        var rgb: UInt64 = 0
+        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
+        
+        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let b = CGFloat(rgb & 0x0000FF) / 255.0
+        
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+}
